ec2010828eee614b7077c5529704412dcc55255c
Matt Hoffner <matthoffner@gmail.com>
2020-01-03 07:13:41 +0000

    support deletes

--- routes/home/index.js
+++ routes/home/index.js
@@ -14,13 +14,14 @@ const [codeState, setCodeState] = React.useState("");
 const [saved, saveCode] = React.useState([]);
 const [filename, setFilename] = React.useState('filename.js');
 const [folder, setFolder] = React.useState([]);
+const [editor, setShowEditor] = React.useState(true);
 const saveSnippet = async () => {
   if (!codeState) {
     return;
   }
   saveCode([codeState,...saved]);
   await savedCode.set(filename, codeState);
-  console.log(savedCode);
+  showSaved();
 }
 const showSaved = async () => {
   const files = await savedCode.keys();
@@ -33,6 +34,10 @@ const showSaved = async () => {
 const loadCode = async (f) => {
   const code = await savedCode.get(f);
   setCodeState(code);
+}
+const deleteCode = async (f) => {
+  const code = await savedCode.delete(f);
+  showSaved();
 }
 const snippet = `
 function Component() {
@@ -53,14 +58,18 @@ return html`
   <div>
     <${LiveProvider} noInline={true} code=${codeState || snippet}>
       <${LivePreview} />
-      <${LiveEditor} value=${codeState || snippet} onValueChange="${e => setCodeState(e)}" />
+      ${editor && html`<${LiveEditor} value=${codeState || snippet} onValueChange="${e => setCodeState(e)}" />`}
       <${LiveError} />
-      <button onClick="${() => saveSnippet()}">Save</button>
-      <input onChange="${e => setFilename(e.target.value)}";
-     }} value=${filename}></input>
-      <button onClick="${() => showSaved()}">Show</button>
       <div>
-        ${folder.map(filename => html`<li><a onClick="${() => loadCode(filename)}">${filename}</a></li>`)}
+        <button onClick="${() => setShowEditor(!editor)}">${editor ? html`Hide` : html`Show`} Editor</button>
+      </div>
+      <button onClick="${() => saveSnippet()}">Save</button>
+      <input onChange="${e => setFilename(e.target.value)}" value=${filename}></input>
+      <div>
+        <button onClick="${() => showSaved()}">My Files</button>
+	<ul>
+	  ${folder.map(filename => html`<li><a onClick="${() => loadCode(filename)}" href="javascript:;">${filename}</a> (<a onClick="${() => deleteCode(filename)}" href="javascript:;">remove</a>)</li>`)}
+	</ul>
       </div>
     </${LiveProvider}>
   </div>
