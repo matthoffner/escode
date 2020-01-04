7f3115609dfca2aeb7bf1e017801b55e2a254875
Matt Hoffner <matthoffner@gmail.com>
2020-01-04 05:40:20 +0000

    just using kv polyfill by default, update sw to cache more

--- sw.js
+++ sw.js
@@ -1,39 +1,67 @@
-const VERSION = 4;
-const CACHENAME = `escode-pwa-v${VERSION}`;
+/**
+ *  @Lifecycle Activate
+ *  New one activated when old isnt being used.
+ *
+ *  waitUntil(): activating ====> activated
+ */
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim())
+})
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHENAME).then((cache) => {
-      return cache.addAll([
-        '/',
-        './web_modules/react-live.js',
-        './index.html',
-        './index.js',
-        './routes/notfound/index.js',
-        './routes/home/index.js',
-        'https://unpkg.com/es-react'
-      ]);
-    })
-  );
-});
+const RUNTIME = 'escode'
+const HOSTNAME_WHITELIST = [
+  self.location.hostname,
+  'unpkg.com'
+]
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => cacheName.startsWith('escode-pwa-') && cacheName !== CACHENAME)
-          .map(cacheName => caches.delete(cacheName))
-      )
-    })
-  )
-});
+// The Util Function to hack URLs of intercepted requests
+const getFixedUrl = (req) => {
+  var now = Date.now()
+  var url = new URL(req.url)
 
-self.addEventListener('fetch', async (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(caches.match('/'));
-    return;
+  // 1. fixed http URL
+  // Just keep syncing with location.protocol
+  // fetch(httpURL) belongs to active mixed content.
+  // And fetch(httpRequest) is not supported yet.
+  url.protocol = self.location.protocol
+
+  // 2. add query for caching-busting.
+  // Github Pages served with Cache-Control: max-age=600
+  // max-age on mutable content is error-prone, with SW life of bugs can even extend.
+  // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
+  // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
+  if (url.hostname === self.location.hostname) {
+    url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
   }
+  return url.href
+}
 
-  event.respondWith(caches.match(event.request).then(res => res || fetch(event.request)))
-});
+self.addEventListener('fetch', event => {
+  // Skip some of cross-origin requests, like those for Google Analytics.
+  if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
+    // Stale-while-revalidate
+    // similar to HTTP's stale-while-revalidate: https://www.mnot.net/blog/2007/12/12/stale
+    // Upgrade from Jake's to Surma's: https://gist.github.com/surma/eb441223daaedf880801ad80006389f1
+    const cached = caches.match(event.request)
+    const fixedUrl = getFixedUrl(event.request)
+    const fetched = fetch(fixedUrl, { cache: 'no-store' })
+    const fetchedCopy = fetched.then(resp => resp.clone())
+
+    // Call respondWith() with whatever we get first.
+    // If the fetch fails (e.g disconnected), wait for the cache.
+    // If there’s nothing in cache, wait for the fetch.
+    // If neither yields a response, return offline pages.
+    event.respondWith(
+      Promise.race([fetched.catch(_ => cached), cached])
+        .then(resp => resp || fetched)
+        .catch(_ => { /* eat any errors */ })
+    )
+
+    // Update the cache with the version we fetched (only for ok status)
+    event.waitUntil(
+      Promise.all([fetchedCopy, caches.open(RUNTIME)])
+        .then(([response, cache]) => response.ok && cache.put(event.request, response))
+        .catch(_ => { /* eat any errors */ })
+    )
+  }
+})
